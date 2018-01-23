35117aa3374113e5f1a199dea03ae1f37adfe9fb
Jeff Hostetler <hostetjm@gmail.com>
2018-01-22 17:41:31 -0700

    Add distillery and release script. Will set up the amazon web stuff later

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mix deps.get --only prod
+MIX_ENV=prod mix compile
+cd assets
+node node_modules/brunch/bin/brunch build --production
+cd ..
+mix phx.digest
+
+MIX_ENV=prod mix release
