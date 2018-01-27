ef672724fc9d5485bdec355833f093617ba6e019
Jeff Hostetler <hostetjm@gmail.com>
2018-01-27 13:34:35 -0700

    fix release

--- release.sh
+++ release.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-mix deps.get --only prod
-MIX_ENV=prod mix compile
-cd assets
-node node_modules/brunch/bin/brunch build --production
-cd ..
-mix phx.digest
+#mix deps.get --only prod
+#MIX_ENV=prod mix compile
+#cd assets
+#node node_modules/brunch/bin/brunch build --production
+#cd ..
+#mix phx.digest
+#
+#MIX_ENV=prod mix release --env=prod
 
-MIX_ENV=prod mix release --env=prod
+git push gigalixir master -f
