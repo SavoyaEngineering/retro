321f17b05594dfae6c2c44e3c7f9ce927c67e78d
Jeff Hostetler <hostetjm@gmail.com>
2018-02-08 09:14:58 -0700

    WIP2

--- assets/js/new-room.jsx
+++ assets/js/new-room.jsx
@@ -2,64 +2,59 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 
 class NewRoom extends React.Component<any, any> {
-    render() {
-        var type: string = "TSX";
-        return (
-            <div>
-                <div className='jumbotron'>
-                    <h2>Create a retro for you and your friends</h2>
-                </div>
-                <form>
-                    <div className="form-group">
-                        <label for="room-name">
-                            Name:
-                        </label>
-                        <input type="text" name="room-name" className="form-control" placeholder="Retro Name"/>
-                    </div>
-                    <div className="form-group">
-                        <label for="room-password">
-                            Password:
-                        </label>
-                        <input type="text" name="room-password" className="form-control" placeholder="Password"/>
-                    </div>
-                    <input type="submit" value="Submit" className="btn btn-primary"/>
-                </form>
-            </div>
-        )
-    }
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      roomName: '',
+      roomPassword: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(type: string, event) {
+    this.setState({[type]: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('name was ' + this.state.roomName + ' and password was ' + this.state.roomPassword);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='jumbotron'>
+          <h2>Create a retro for you and your friends</h2>
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="room-name">
+              Name:
+            </label>
+            <input type="text" name="room-name" className="form-control" placeholder="Retro Name"
+                   value={this.state.roomName} onChange={this.handleChange.bind(this, 'roomName')}/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="room-password">
+              Password:
+            </label>
+            <input type="text" name="room-password" className="form-control" placeholder="Password"
+                   value={this.state.roomPassword} onChange={this.handleChange.bind(this, 'roomPassword')}/>
+          </div>
+          <input type="submit" value="Submit" className="btn btn-primary"/>
+        </form>
+      </div>
+    )
+  }
 }
 
-
-// <div class="jumbotron">
-//     <h2>Create a retro for you and your friends</h2>
-//     <div id="react-new-room"></div>
-// </div>
-//
-// <%= form_for @changeset, room_path(@conn, :create), fn f -> %>
-// <%= if @errors do %>
-// <%= for error <- @changeset.errors do %>
-// <div class="text-danger"><%= readable_error(error) %></div>
-//     <% end %>
-//     <% end %>
-//     <form>
-//     <div class="form-group">
-//     <label for="roomName">Name</label>
-// <%= text_input f, :name, class: "form-control", id: "roomName", placeholder: "Retro Name"%>
-// </div>
-// <div class="form-group">
-//     <label for="roomPassword">Password</label>
-// <%= text_input f, :password, class: "form-control", id: "roomPassword", type: "passwword", placeholder: "password"%>
-// </div>
-// <button type="submit" class="btn btn-primary">Create</button>
-//     </form>
-//     <% end %>
-
-
 export default function render(node) {
-    ReactDOM.render(
-        (<div>
-            <NewRoom/>
-        </div>),
-        node
-    )
+  ReactDOM.render(
+    (<div>
+      <NewRoom/>
+    </div>),
+    node
+  )
 }
